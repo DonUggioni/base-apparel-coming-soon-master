4509e63619efee98193dc6c1011df4811db44c5b
Renan Tiscoski <renan.uggioni@gmail.com>
2022-06-04 12:12:12 +0100

    Work on media queries, script and main CSS

--- js/main.js
+++ js/main.js
@@ -4,11 +4,35 @@ const formEL = document.getElementById('form');
 const emailEL = document.getElementById('email');
 const inputGroupEL = document.querySelector('.input__group');
 const errorMessage = document.querySelector('.error__message');
+const heroDesktop = document.querySelector('.hero__img');
+const heroMobile = document.querySelector('.hero__img--mobile');
 
 formEL.addEventListener('submit', function (e) {
   e.preventDefault();
 
   validateEmail();
+});
+
+// Check window size on load
+window.addEventListener('load', function () {
+  if (this.window.innerWidth <= 904) {
+    heroDesktop.classList.add('hero__img--hidden');
+    heroMobile.classList.remove('hero__img--hidden');
+  } else {
+    heroDesktop.classList.remove('hero__img--hidden');
+    heroMobile.classList.add('hero__img--hidden');
+  }
+});
+
+// Check for window resize
+window.addEventListener('resize', function () {
+  if (this.window.innerWidth <= 904) {
+    heroDesktop.classList.add('hero__img--hidden');
+    heroMobile.classList.remove('hero__img--hidden');
+  } else {
+    heroDesktop.classList.remove('hero__img--hidden');
+    heroMobile.classList.add('hero__img--hidden');
+  }
 });
 
 // Email validation
